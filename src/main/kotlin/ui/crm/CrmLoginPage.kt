0e9e0b1bf878f917cfa6182182f62adef2430a82
kirill-predkel-edu <k.predkel@gmail.com>
2021-11-22 16:13:11 +0300

    Add additional checks for elements

--- src/main/kotlin/ui/crm/CrmLoginPage.kt
+++ src/main/kotlin/ui/crm/CrmLoginPage.kt
@@ -5,9 +5,7 @@ import com.codeborne.selenide.Selenide.open
 import com.codeborne.selenide.SelenideElement
 import org.openqa.selenium.By
 import ui.elements.button.Button
-import ui.elements.input.InputCaptcha
-import ui.elements.input.InputEmail
-import ui.elements.input.InputPassword
+import ui.elements.input.InputField
 
 internal class CrmLoginPage {
   private val emailField: SelenideElement = element(By.cssSelector("#username"))
@@ -25,15 +23,15 @@ internal class CrmLoginPage {
   }
 
   fun inputEmail(login: String) {
-    InputEmail.inputEmail(emailField, login)
+    InputField.inputField(emailField, login)
   }
 
   fun inputPassword(password: String) {
-    InputPassword.inputPassword(passwordField, password)
+    InputField.inputField(passwordField, password)
   }
 
   fun inputCaptcha(captcha: String) {
-    InputCaptcha.inputCaptcha(captchaField, captcha)
+    InputField.inputField(captchaField, captcha)
   }
 
   fun clickSubmitButton() {
