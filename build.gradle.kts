bc332fe2c926b8442c98b2589c91fae7338d081d
kirill-predkel-edu <86263425+kirill-predkel-edu@users.noreply.github.com>
2021-12-06 14:43:39 +0300

    Add locators (#8)
    * Add locators
    
    * add okhttp dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ repositories {
 
 val jacksonVersion: String by project
 val junitJupiterVersion: String by project
+val okhttpVersion: String by project
 val selenideVersion: String by project
 val webDriverManagerVersion: String by project
 
@@ -23,7 +24,7 @@ dependencies {
     implementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
     implementation("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
     implementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
-
+    implementation("com.squareup.okhttp3:okhttp:$okhttpVersion")
     implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonVersion")
