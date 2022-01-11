2b8f7752ef34c8804ee1a1254992d415f3f93628
kirill-predkel-edu <k.predkel@gmail.com>
2022-01-11 16:03:31 +0300

    change variable type for test

--- src/test/kotlin/api/TafContextTest.kt
+++ src/test/kotlin/api/TafContextTest.kt
@@ -10,13 +10,15 @@ import org.junit.jupiter.api.Test
 internal class TafContextTest : BaseTest() {
   private val authUserHeaderName: String = "AuthUser"
   private lateinit var authUserCookieFromContext: String
+  private lateinit var authUserCookieFromResponse: String
 
   @Test
   fun `TAF Context - Store AuthUser to context and verify token is stored`() {
     val controller = RegistrationController(config.host)
     val response: RetrofitResponse = controller.getRegistrationResponse()
+
     authUserCookieFromContext = getAuthUser().toString()
-    val authUserCookieFromResponse: String? = response.getCookieByName(authUserHeaderName)
+    authUserCookieFromResponse = response.getCookieByName(authUserHeaderName).toString()
     assertEquals(authUserCookieFromResponse, authUserCookieFromContext, "AuthUser from context and response aren't " +
         "equal")
   }
