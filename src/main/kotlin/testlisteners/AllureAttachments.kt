5576cf7af6c0542364d8585dccc073a8e45745cd
kirill-predkel-edu <k.predkel@gmail.com>
2022-01-21 16:54:48 +0300

    wrap addlogs call with try-catch

--- src/main/kotlin/testlisteners/AllureAttachments.kt
+++ src/main/kotlin/testlisteners/AllureAttachments.kt
@@ -3,12 +3,16 @@ package testlisteners
 import com.codeborne.selenide.Selenide
 import io.qameta.allure.Allure
 import io.qameta.allure.Attachment
+import mu.KotlinLogging
 import org.apache.commons.io.FileUtils
 import org.openqa.selenium.OutputType
 import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
 
 class AllureAttachments {
   private val logPath: String = "testFile.log"
+  private val logger = KotlinLogging.logger {}
 
   @Attachment
   fun addScreenshotToAllureReport() {
@@ -18,7 +22,13 @@ class AllureAttachments {
 
   @Attachment
   fun addLogToAllureReport() {
-    val log = File(logPath)
-    Allure.addAttachment("LogFile", FileUtils.openInputStream(log))
+    try {
+      val log = File(logPath)
+      Allure.addAttachment("LogFile", FileUtils.openInputStream(log))
+    } catch (e: FileNotFoundException) {
+      logger.error { "LogFile isn't found" }
+    } catch (e: IOException) {
+      logger.error { "LogFile can't be read" }
+    }
   }
 }
